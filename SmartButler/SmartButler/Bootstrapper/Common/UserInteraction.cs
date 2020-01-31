2846645198d262037c9a6d0751c3d6b5e08333c0
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2020-01-31 20:34:20 +0100

    imported acr userdialogs nuget

--- SmartButler/SmartButler/Bootstrapper/Common/UserInteraction.cs
+++ SmartButler/SmartButler/Bootstrapper/Common/UserInteraction.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Acr.UserDialogs;
 using SmartButler.Logic.Interfaces;
 using Xamarin.Forms;
 
@@ -6,19 +8,21 @@ namespace SmartButler.Bootstrapper.Common
 {
 	public class UserInteraction : IUserInteraction
 	{
+		private static readonly IUserDialogs _dialogs = UserDialogs.Instance;
+
 		public Task DisplayAlertAsync(string title, string message, string cancel)
 		{
-			return ((App)Application.Current).MainPage.DisplayAlert(title, message, cancel);
+			return _dialogs.AlertAsync(message, title, cancel);
 		}
 
 		public Task<bool> DisplayAlertAsync(string title, string message, string accept, string cancel)
 		{
-			return ((App)Application.Current).MainPage.DisplayAlert(title, message, accept, cancel);
+			return _dialogs.ConfirmAsync(title, message, accept, cancel);
 		}
 
 		public async Task<string> DisplayActionSheetAsync(string title, string cancel, string destruction, params string[] buttons)
 		{
-			return await ((App)Application.Current).MainPage.DisplayActionSheet(title, cancel, destruction, buttons);
+			return await _dialogs.ActionSheetAsync(title, cancel, destruction, buttons: buttons);
 		}
 
 	}
